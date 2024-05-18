8fb5e6ced85058c16dddc2e1bda980620c011311
ZakyHermawan <zaky.hermawan9615@gmail.com>
2024-05-18 20:37:07 +0700

    finish workflows parsing
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- core/blocks/options.py
+++ core/blocks/options.py
@@ -1,88 +1,126 @@
 from . import Block, register_build_in
 from ._templates import MakoTemplates
 from ._build import build_params
+from ..base import Element
+
+from collections import OrderedDict, defaultdict
 
 templates = MakoTemplates()
 
 @register_build_in
 class Options(Block):
-  key = 'options'
-  label = 'Options'
+    """
+    Implementation of Options block for codegen purpose
+    This block will be used to select workflow and provide informations for code generator
+    """
+    key = 'options' # block id
+    label = 'Options' # block name
 
-  parameters_data = build_params(
-    params_raw=[
-      dict(id='title',
-        label='Title',
-        dtype='string',
-        hide="${ ('none' if title else 'part') }",
-      ),
-      dict(id='author',
-        label='Author',
-        dtype='string',
-        hide="${ ('none' if author else 'part') }",
-      ),
-      dict(id='copyright',
-        label='Copyright',
-        dtype='string',
-        hide="${ ('none' if copyright else 'part') }",
-      ),
-      dict(id='description',
-        label='description',
-        dtype='string',
-        hide="${ ('none' if description else 'part') }",
-      ),
-      dict(id='output_language',
-        label='Output Language',
-        dtype='enum',
-        default='python',
-        options=['python', 'cpp', 'asm'],
-        option_labels=['Python', 'C++', 'Assembly__'],
-        hide="${ ('none' if generate_options else 'part') }",
-      ),
-      dict(id='generate_options',
-        label='Generate Options',
-        dtype='enum',
-        default='qt_gui',
-        options=['qt_gui', 'bokeh_gui', 'no_gui', 'hb', 'hb_qt_gui'],
-        option_labels=['QT GUI', 'Bokeh GUI', 'No GUI', 'Hier Block', 'Hier Block (QT GUI)'],
-      ),
-    ],
-    have_inputs=False,
-    have_outputs=False,
-    flags=Block.flags,
-    block_id=key,
-  )
-
-  def __init__(self, parent):
-    super().__init__(parent)
-    parent_platform = parent.parent_platform
-
-    self.workflows = parent_platform.workflow_manager.workflows
-    self.codegen_options = dict()
-    self.parse_workflows()
-
-    output_language = dict(
-      id='output_language',
-      label='Output Language',
-      dtype='enum',
-      default='python',
-      options=self.codegen_options.keys(),
-      option_labels=self.codegen_options.keys(),
-      hide="${ ('none' if generate_options else 'part') }",
+    parameters_data = build_params(
+        params_raw=[
+          dict(id='title',
+            label='Title',
+            dtype='string',
+            hide="${ ('none' if title else 'part') }",
+          ),
+          dict(id='author',
+            label='Author',
+            dtype='string',
+            hide="${ ('none' if author else 'part') }",
+          ),
+          dict(id='copyright',
+            label='Copyright',
+            dtype='string',
+            hide="${ ('none' if copyright else 'part') }",
+          ),
+          dict(id='description',
+            label='description',
+            dtype='string',
+            hide="${ ('none' if description else 'part') }",
+          ),
+          dict(id='output_language',
+            label='Output Language',
+            dtype='enum',
+            default='python',
+            options=['python'],
+            option_labels=['Python'],
+            option_attributes=defaultdict(dict), # delete soon, i think its unnecessary
+            hide="${ ('none' if generate_options else 'part') }",
+          ),
+          dict(id='generate_options',
+            label='Generate Options',
+            dtype='enum',
+            default='qt_gui',
+            options=['qt_gui'],
+            option_labels=['QT GUI'],
+          ),
+        ],
+        have_inputs=False,
+        have_outputs=False,
+        flags=Block.flags,
+        block_id=key,
     )
-    print(self.parameters_data[6])
-    self.parameters_data[6] = output_language # how to update this on gui ?
-    print(self.parameters_data[6])
 
-  def parse_workflows(self):
-    # read all workflow yml file
-    # for each workflow, get it's parameters
-    for workflow in self.workflows:
-      output_language = workflow.output_language
-      if output_language not in self.codegen_options.keys():
-        self.codegen_options[output_language] = []
-      if workflow.generator_options not in self.codegen_options[output_language]:
-        self.codegen_options[output_language].append(workflow.generator_options)
-  
-  cpp_templates = MakoTemplates(includes=['#include <gnuradio/topblock.h>'])
-  file_format=1
+    def __init__(self, parent):
+      super().__init__(parent)
+      parent_platform = parent.parent_platform
+
+      self.workflows = parent_platform.workflow_manager.workflows # get all available workflow objects
+      self.codegen_options = dict() # key: output_language, val: list of generator_options
+      self.parse_workflows()
+
+      default_output_language = self.params['output_language']
+
+      tmp_dct = OrderedDict()
+      tmp_dct.attributes = defaultdict(dict)
+      for output_language, output_language_label in self.codegen_options.keys():
+        tmp_dct[output_language] = output_language_label
+      default_output_language.options = tmp_dct
+      self.params['output_language'] = default_output_language
+
+    def rewrite(self):
+        """
+        Update generated_options each time the value of output_language is changed
+        """
+        Element.rewrite(self)
+
+        choosen_language = self.params['output_language'].value
+        output_language_pair = ()
+        for output_language, output_language_label in self.codegen_options.keys():
+            if choosen_language == output_language:
+              output_language_pair = (output_language, output_language_label)
+              break
+
+        list_of_generated_options_pair = self.codegen_options[output_language_pair]
+
+        tmp = OrderedDict()
+        tmp.attributes = defaultdict(dict)
+        for generated_options, generated_options_label in list_of_generated_options_pair:
+            tmp[generated_options] = generated_options_label
+        self.params['generate_options'].options = tmp
+        super(Options, self).rewrite()
+        # probem: after self.params['generate_options'] get changed, its not getting rendered
+        # how to manually render self.params['generate_options']
+
+    def parse_workflows(self):
+        """
+        Read all workflow yml file
+        for each workflow, use pair for output_language, generator_options and their label
+        then, put it on codegen_options
+
+        structure of codegen_options will be
+        codegen_options = {
+            ...
+            (output_language, output_language_label): [(generated_options, generated_options_label), ..]
+        }
+        """
+        for workflow in self.workflows:
+            output_language_pair = (workflow.output_language, workflow.output_language_label)
+            generator_options_pair = (workflow.generator_options, workflow.generator_options_label)
+            if output_language_pair not in self.codegen_options.keys():
+                self.codegen_options[output_language_pair] = []
+            if generator_options_pair not in self.codegen_options[output_language_pair]:
+                self.codegen_options[output_language_pair].append(generator_options_pair)
+
+    cpp_templates = MakoTemplates(includes=['#include <gnuradio/topblock.h>'])
+    file_format=1
--- src/gnuradio_companion/core/workflow.py
+++ src/gnuradio_companion/core/workflow.py
@@ -1,43 +1,65 @@
 import logging
 import yaml
 
+# todo 
+
 logger = logging.getLogger(__name__)
 
 class Workflow:
-  """
-  Workflow class is used to parse workflow file
-  """
-  def __init__(self, *args, **kwargs):
-    if len(args) == 1:
-      with open(args[0], 'r') as wf:
-        self.workflow_params = yaml.safe_load(wf)
-    else:
-      self.workflow_params = kwargs
+    """
+    Workflow class is used to parse workflow file
 
-    self.id = self.workflow_params.pop('id')
-    self.label = self.workflow_params.pop('label')
-    self.descripion = self.workflow_params.pop('description')
-    self.output_language = self.workflow_params.pop('output_language')
-    self.generator_class = self.workflow_params.pop('generator_class')
-    self.generator_options = self.workflow_params.pop('generator_options')
-    self.context = self.workflow_params # additional workflow parameters
+    Attributes:
+        id (str): unique name of workflow
+        descripton (str): detailed information of workflow
+        output_language (str): target language
+        output_language_label (str): Information for users about the target language of the workflow
+        generator_class (str): Name of the code generator class
+        generator_options (str): Used to select a workflow
+        generator_options_label (str): Information for users to select a workflow
+        context (dict): additional workflow parameters
+    """
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1:
+            with open(args[0], 'r') as wf:
+                self.workflow_params = yaml.safe_load(wf)
+        else:
+            self.workflow_params = kwargs
+
+        self.id = self.workflow_params.pop('id')
+        self.descripion = self.workflow_params.pop('description')
+        self.output_language = self.workflow_params.pop('output_language')
+        self.output_language_label = self.workflow_params.pop('output_language_label')
+        self.generator_class = self.workflow_params.pop('generator_class')
+        self.generator_options = self.workflow_params.pop('generator_options')
+        self.generator_options_label = self.workflow_params.pop('generator_options_label')
+        self.context = self.workflow_params # additional workflow parameters
 
 
 class WorkflowManager:
-  """
-  WorkflowManager will be used by platform to load workflow files
-  This class hold all workflows
-  """
-  def __init__(self):
-    self.workflows = []
+    """
+    WorkflowManager will be used by platform to load workflow files
+    This class hold all workflows
 
-  def load_workflow(self, _, filepath):
-    log = logger.getChild('workflow_manager')
-    log.setLevel(logging.DEBUG)
-    wf = Workflow(filepath)
-    if wf in self.workflows:
-      print("File already parsed")
-      return
-    else:
-      self.workflows.append(wf)
+    Attributes:
+        workflows: all available workflow objects
+    """
+    def __init__(self):
+        self.workflows = []
+
+    def load_workflow(self, _, filepath) -> None:
+        """
+        This function will be called by platform on app initialization
+        to load all availble workflows
+        Args:
+        _ (any): Unused
+        filepath (str): path of workflow file
+        """
+        log = logger.getChild('workflow_manager')
+        log.setLevel(logging.DEBUG)
+        wf = Workflow(filepath)
+        if wf in self.workflows:
+            print("File already parsed")
+        else:
+            self.workflows.append(wf)
 
