2c1fd47cb5c899b4f1efbd6066b2f444be9af808
ZakyHermawan <zaky.hermawan9615@gmail.com>
2024-06-05 22:12:45 +0700

    Remove unnecessary files and add GRC_WORKFLOWS_DIR variable into CMakeLists.txt
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- gui_qt/Platform.py
+++ gui_qt/Platform.py
@@ -16,7 +16,6 @@ from .Config import Config
 from .components.canvas.flowgraph import Flowgraph
 from .components.canvas.block import Block
 from .components.canvas.port import Port
-# from .components.canvas.param import Param
 from .components.canvas.connection import Connection
 from ..core.platform import Platform as CorePlatform
 
