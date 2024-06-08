73d0a000ca72bd47c7d56cbbc935f605b2cf1030
ZakyHermawan <zaky.hermawan9615@gmail.com>
2024-06-08 21:37:40 +0700

    fix parameter hide problem
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- grc/core/blocks/options.py
+++ grc/core/blocks/options.py
@@ -4,7 +4,6 @@ from ._build import build_params
 from ..base import Element
 from ..params import Param
 
-from copy import deepcopy
 from collections import OrderedDict, defaultdict
 import typing
 
@@ -26,39 +25,48 @@ class Options(Block):
         self.raw_params_data = [
             dict(id='title',
                 label='Title',
+                workflow='',
                 dtype='string',
                 hide="${ ('none' if title else 'part') }",
             ),
             dict(id='author',
                 label='Author',
+                workflow='',
                 dtype='string',
                 hide="${ ('none' if author else 'part') }",
             ),
             dict(id='copyright',
                 label='Copyright',
+                workflow='',
                 dtype='string',
                 hide="${ ('none' if copyright else 'part') }",
             ),
             dict(id='description',
                 label='description',
+                workflow='',
                 dtype='string',
                 hide="${ ('none' if description else 'part') }",
             ),
             dict(id='output_language',
                 label='Output Language',
+                workflow='',
                 dtype='enum',
                 options=[],
                 option_labels=[],
+                hide="none"
             ),
             dict(id='generate_options',
                 label='Generate Options',
+                workflow='',
                 dtype='enum',
                 default='qt_gui',
                 options=['qt_gui', 'bokeh_gui'],
                 option_labels=['QT GUI', 'Bokeh GUI'],
+                hide="none"
             ),
             dict(id='gen_linking',
                 label='Linking',
+                workflow='',
                 dtype='enum',
                 default='dynamic',
                 options=['dynamic', 'static'],
@@ -67,6 +75,7 @@ class Options(Block):
             ),
             dict(id='thread_safe_setters',
                 label='Thread-safe setters',
+                workflow='',
                 category='Advanced',
                 dtype='enum',
                 options=['', '1'],
@@ -75,6 +84,7 @@ class Options(Block):
             ),
             dict(id='catch_exceptions',
                 label='Catch Block Exceptions',
+                workflow='',
                 category='Advanced',
                 dtype='enum',
                 default='True',
@@ -84,24 +94,35 @@ class Options(Block):
             ),
             dict(id='hier_block_src_path',
                 label='Hier Block Source Path',
+                workflow='',
                 category='Advanced',
                 dtype='string',
                 options='.:',
                 hide="part",
             ),
             dict(id='generator_class_name',
+                workflow='',
                 dtype='string',
-                hide="all"
+                hide="all",
             ),
             dict(id='generator_module',
+                workflow='',
                 dtype='string',
-                hide="all"
+                hide="all",
+            ),
+            dict(id='current_workflow',
+                workflow='',
+                dtype='string',
+                default='',
+                hide="all",
             ),
         ]
         self.parent_platform = parent.parent_platform
         self.workflows = self.parent_platform.workflow_manager.workflows # get all available workflow objects
         self.codegen_options = dict() # key: output_language, val: list of generator_options
         self.current_workflow = None
+        self.previous_params = None
+        self.workflow_params = []
 
         self.parse_workflows()
 
@@ -127,7 +148,7 @@ class Options(Block):
         )
 
         super().__init__(parent)
-        self.initial_params = deepcopy(self.params)
+        self.build_workflow_params()
 
     def insert_grc_parameters(self, grc_parameters):
         """
@@ -176,8 +197,7 @@ class Options(Block):
 
         self.update_current_workflow()
         self.update_generator_class()
-        self.update_parameters_from_workflow()
-        self.update_asserts()
+        self.update_params_hide()
 
         Element.rewrite(self)
         super().rewrite()
@@ -202,13 +222,17 @@ class Options(Block):
             if generator_options_pair not in self.codegen_options[output_language_pair]:
                 self.codegen_options[output_language_pair].append(generator_options_pair)
 
-    def update_parameters_from_workflow(self) -> None:
-        new_params_from_workflow = self.current_workflow.parameters
-        if new_params_from_workflow == None: # no additional parameter
-            return
+            params = workflow.parameters
+            for param in params:
+                param['workflow'] = workflow.id
+                self.workflow_params.append(param)
 
+    def build_workflow_params(self) -> None:
+        """
+        add params from workflow files into self.params
+        """
         new_params_data = build_params(
-            params_raw=new_params_from_workflow,
+            params_raw=self.workflow_params,
             have_inputs=False,
             have_outputs=False,
             flags=Block.flags,
@@ -216,35 +240,6 @@ class Options(Block):
         )
 
         param_factory = self.parent_platform.make_param
-
-        # uncommenting code below will result on infinite recursion
-        # self.parameters_data = build_params(
-        #     params_raw=self.raw_params_data,
-        #     have_inputs=False,
-        #     have_outputs=False,
-        #     flags=Block.flags,
-        #     block_id=self.key,
-        # )
-        # self.params: typing.OrderedDict[str, Param] = OrderedDict(
-        #     (data['id'], param_factory(parent=self, **data)) for data in self.parameters_data)
-        # self.params['id'].hide = 'part'
-
-        # if code below is getting uncommented instead, it will alsoo, result in infinite recursion
-        # self.params = deepcopy(self.initial_params)
-
-        # this will also result in infinite recursion
-        # od = OrderedDict()
-        # for data in self.parameters_data:
-        #     data['origin'] = self.current_workflow.id
-        #     od[data['id']] = param_factory(parent=self, **data)
-        # self.params = od
-
-        # the infinite recursion happened when the self.params is getting set to another value
-        # ex: self.params = od
-
-        for data in new_params_data:
-            data['workflow_origin'] = self.current_workflow.id
-
         new_params: typing.OrderedDict[str, Param] = (OrderedDict(
             (data['id'], param_factory(parent=self, **data)) for data in new_params_data))
 
@@ -252,12 +247,25 @@ class Options(Block):
             if not self.params.get(key):
                 self.params[key] = val
 
-    def update_asserts(self) -> None:
-        asserts = self.current_workflow.asserts
-        if asserts == None:
+    def update_params_hide(self) -> None:
+        """
+        update hide attributes of self.parameters
+        """
+        new_params_from_workflow = self.current_workflow.parameters
+        if new_params_from_workflow == None: # no additional parameter
+            return
+        if self.params['current_workflow'].get_value() == self.current_workflow.id:
             return
 
-        self.asserts.extend(asserts)
+        additional_params = []
+        for param in self.workflow_params:
+            if param['workflow'] == self.current_workflow.id:
+                additional_params.append(param)
+
+        for param in additional_params:
+            self.params[param['id']].hide = param.get('hide')
+
+        self.params['current_workflow'].value = self.current_workflow.id
 
     def update_current_workflow(self) -> None:
         for workflow in self.workflows:
